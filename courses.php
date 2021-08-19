f0919dd04f7b4dc4158598c3dd9d3fb2835a3be5
EdwardLuoJihan <62911139+EdwardLuoJihan@users.noreply.github.com>
2021-08-18 18:42:18 -0600

    fix

--- courses.php
+++ courses.php
@@ -1,14 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <link rel="stylesheet" href="style.css" />
-        <link rel="stylesheet" href="queries.css" />
-        <title>Kev Web</title>
-    </head>
     <body>
         <nav>
             <div class="navbar__container" id="navbar">
