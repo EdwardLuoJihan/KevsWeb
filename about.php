034cda0b6dab605523a01d79e38ac41fc903784a
EdwardLuoJihan <62911139+EdwardLuoJihan@users.noreply.github.com>
2021-08-20 14:33:01 -0600

    fixed style.css

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <link rel="stylesheet" href="styles/style.css" type="text/css">
+        <link rel="stylesheet" href="style.css" type="text/css">
         <link
         rel="stylesheet"
         href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"
